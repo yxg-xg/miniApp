1aa3227cc4866da48ffc2eb738c8f6311385058d
yxg-xg <923218778@qq.com>
2018-05-16 00:16:23 +0800

    小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,6 +17,9 @@ Page({
       { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'},
       { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'},
       { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'}
+    ],
+    informationMain:[
+      { informationMainText: "房产资讯房产资讯房产资讯房产资讯房产资讯", textSmall:"房产资讯房产资讯房产资讯房产资讯房产资讯"}
     ]
   },
   
