9390a80769e0e3dc69b38a52001ed29a8b170dae
yxg-xg <923218778@qq.com>
2018-05-17 23:15:38 +0800

    2018/5/17

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,10 +13,54 @@ Page({
     interval:3000,
     duration:800,
     recommendImg:[
-      { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg:'优惠政策' },
-      { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'},
-      { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'},
-      { src: '/img/aa.jpg', title: '海南绿地城', small: '金江', price: '10000', discountsMsg: '优惠政策'}
+      {
+        src: '/img/aa.jpg', 
+        title: '海南绿地城', 
+        small: '金江', 
+        price: '10000', 
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ], 
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+      },
     ],
     informationMain:[
       { informationMainText: "房产资讯房产资讯房产资讯房产资讯房产资讯", textSmall:"房产资讯房产产资讯房产资产资讯房产资产资讯房产资"}
--- pages/listHouse/listHouse.js
+++ pages/listHouse/listHouse.js
@@ -8,7 +8,78 @@ Page({
       { filtrateName: "更多" }
     ],
     filtrateIndex:"-1",
-    toggle:true
+    toggle:true,
+    choicenessOne:[
+      { choicenessPrice:"100万以下"},
+      { choicenessPrice: "100-300万" },
+      { choicenessPrice: "300-600万" },
+      { choicenessPrice: "600万" },
+    ],
+    choicenessTwo:[
+      { choicenessAdr:"海景地产"},
+      { choicenessAdr: "养生地产" },
+      { choicenessAdr: "品牌地产" },
+      { choicenessAdr: "精品别墅" },
+
+    ],
+    recommendImg: [
+      { 
+        src: '/img/aa.jpg', 
+        title: '海南绿地城', 
+        small: '金江', 
+        price: '10000', 
+        discountsMsg: '优惠政策', 
+        tally:[
+          { text: "住宅" },
+          { text: "酒店式公寓" }, 
+          { text: "商铺" },
+        ],
+        recommendDiscounts:"保利崖州湾2期新品莱西 瞰海三至四房均价约17500元/平"
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+        recommendDiscounts: "保利崖州湾2期新品莱西 瞰海三至四房均价约17500元/平"
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+        recommendDiscounts: "保利崖州湾2期新品莱西 瞰海三至四房均价约17500元/平"
+      },
+      {
+        src: '/img/aa.jpg',
+        title: '海南绿地城',
+        small: '金江',
+        price: '10000',
+        discountsMsg: '优惠政策',
+        tally: [
+          { text: "住宅" },
+          { text: "酒店式公寓" },
+          { text: "商铺" },
+        ],
+        recommendDiscounts: "保利崖州湾2期新品莱西 瞰海三至四房均价约17500元/平"
+      }
+    ],
+    informationMain: [
+      { informationMainText: "房产资讯房产资讯房产资讯房产资讯房产资讯", textSmall: "房产资讯房产产资讯房产资产资讯房产资产资讯房产资" }
+    ]
+
   },
   filtrateClick(event){
     let idx = event.currentTarget.dataset.index;
@@ -16,7 +87,7 @@ Page({
     let that = this;
     that.setData({
       filtrateIndex: idx, 
-      toggle: false
+     // toggle: false
     })
     //that.setData(that.data.toggle = !that.data.toggle)
     
