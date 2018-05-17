116dec021c8729f5a5838e9d75b44aacb5ab6076
yxg-xg <923218778@qq.com>
2018-05-17 18:50:38 +0800

    2018.5.17

--- pages/listHouse/listHouse.js
+++ pages/listHouse/listHouse.js
@@ -7,16 +7,25 @@ Page({
       { filtrateName: "户型" },
       { filtrateName: "更多" }
     ],
-    filtrateIndex:""
+    filtrateIndex:"-1",
+    toggle:true
   },
   filtrateClick(event){
     let idx = event.currentTarget.dataset.index;
-    this.data.filtrateIndex= idx
-    console.log(event)
+    let toggle = event.currentTarget.dataset.toggle;
+    let that = this;
+    that.setData({
+      filtrateIndex: idx, 
+      toggle: false
+    })
+    //that.setData(that.data.toggle = !that.data.toggle)
+    
+    
+    console.log(toggle);
   
   },
   onLoad: function(){
-    console.log(cityList);
+    console.log();
     
   }
 })
