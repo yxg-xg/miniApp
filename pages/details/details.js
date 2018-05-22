06a9e6eb80a44f4e78879ad34379b1df0df490b8
yxg-xg <923218778@qq.com>
2018-05-22 17:22:40 +0800

    2018/5/22

--- pages/details/details.js
+++ pages/details/details.js
@@ -10,7 +10,16 @@ Page({
       { textTitle: "楼盘相册" },
       { textTitle: "在售相册" },
     ],
-    DetailsTabIndex:0
+    DetailsTabIndex:0,
+    DetailsBuildParameter:[
+      { icon: "/img/icon/kaifashang.png",name: "开发商",text:"海南程艳房地产公司"},
+      { icon: "/img/icon/kaifang.png", name: "开盘时间", text: "海南程艳房地产公司" },
+      { icon: "/img/icon/shijian.png", name: "交盘时间", text: "海南程艳房地产公司" },
+      { icon: "/img/icon/loupan.png", name: "项目地址", text: "海南程艳房地产公司" },
+      { icon: "/img/icon/huxing.png", name: "主力户型", text: "海南程艳房地产公司" },
+      { icon: "/img/icon/nianxian.png", name: "产权年限", text: "海南程艳房地产公司" },
+      
+    ]
   },
   DetailsTabTitleClick(event){
     let idx = event.currentTarget.dataset.index;
